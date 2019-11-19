5890e18cbe4ab5ed7f3c7db10668570fb9dae14a
gosho-kazuya <ketsume0211@gmail.com>
2019-11-19 17:36:12 +0900

    add intorp feature

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -3,25 +3,28 @@ import TypedI18n from '..'
 const en = {
   hello: 'Hello',
   goodbye: 'Goodbye',
+  helloButGoodbye: t => `${t.trans.hello}, but ${t.trans.goodbye}`,
 }
 
 const ja = {
   hello: 'こんにちは',
   goodbye: 'さようなら',
+  helloButGoodbye: t => `${t.trans.hello} ですが ${t.trans.goodbye}`,
 }
 
 type Lang = 'en' | 'ja'
 
 test('TypedI18n', () => {
-  const t = new TypedI18n<Lang, typeof ja>()
-  t.addLocale('ja', ja)
-  t.addLocale('en', en)
-
-  t.setLocale('ja')
-  expect(t.trans.hello).toBe('こんにちは')
+  const t = new TypedI18n<Lang, typeof en>()
+    .addLocale('en', en)
+    .addLocale('ja', ja)
 
   t.setLocale('en')
   expect(t.trans.hello).toBe('Hello')
+  expect(t.trans.helloButGoodbye).toBe('Hello, but Goodbye')
+
+  t.setLocale('ja')
+  expect(t.trans.hello).toBe('こんにちは')
 
   // This throws an error:
   // t.setLocale('cn')
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,34 @@
+type TransFn<L extends string, T> = (t: TypedI18n<L, T>) => string
+
+interface TransFnWithKey<L extends string, T> {
+  [key: string]: string | TransFn<L, T>
+}
+
+type Trans<L extends string, T> = object & T & TransFnWithKey<L, T>
+
+function transform<L extends string, T extends {}>(
+  t: TypedI18n<L, T>,
+  obj: T,
+): Trans<L, T> {
+  return Object.keys(obj).reduce((car, key) => {
+    const value = (obj as any)[key]
+    if (typeof value === 'string') {
+      return { ...car, [key]: value }
+    }
+    return { ...car, [key]: value(t) }
+  }, {} as T)
+}
+
 export class TypedI18n<L extends string, T> {
   locale!: L
-  transMap = new Map<L, T>()
+  transMap = new Map<L, T & TransFnWithKey<L, T>>()
 
-  addLocale(lang: L, trans: T) {
+  addLocale(lang: L, trans: Trans<L, T>): this {
     this.transMap.set(lang, trans)
+    const interop = transform<L, T>(this, trans)
+    console.log(interop)
+    this.transMap.set(lang, interop)
+    return this
   }
 
   setLocale(locale: L) {
