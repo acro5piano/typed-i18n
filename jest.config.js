03130286cb4a3545df08afac58dd14f65d469292
gosho-kazuya <ketsume0211@gmail.com>
2020-03-02 20:17:37 +0900

    add each tsconfig

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,8 @@ module.exports = {
     '^.+\\.tsx?$': 'ts-jest',
   },
   timers: 'fake',
+  testRegex: '(/__tests__/.*|\\.(test|spec))\\.(ts|tsx|js)$',
+  testPathIgnorePatterns: ['.*build.*', '\\.snap$', 'node_modules'],
   globals: {
     'ts-jest': {
       diagnostics: {
